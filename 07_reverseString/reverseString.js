5adf1165d4f2e197437fe3dcf45d6c5243d242be
wilkye <wilkylegreer@gmail.com>
2025-10-20 15:58:02 -0600

    Exercise 07 completed

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let revString = "";
+    for (let i = 0; i < str.length; i++) {
+        revString += str[str.length - 1 - i];
+    }
+    return revString;
 };
 
 // Do not edit below this line
