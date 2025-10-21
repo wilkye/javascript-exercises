4355ba6b3cddc996df63de435f2aca848a4daad5
wilkye <wilkylegreer@gmail.com>
2025-10-20 20:33:25 -0600

    Exercise 09 completed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    let finalNum = 0;
+    let upper = 0;
+    let lower = 0;
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
 
+    if (a < b) {
+        upper = b;
+        lower = a;
+    } else {
+        upper = a;
+        lower = b;
+    }
+
+    if (typeof a !== 'number') {
+        return "ERROR";
+    }
+    for (let i = lower; i <= upper; i++) {
+        finalNum += i;
+    }
+    return finalNum;
 };
 
 // Do not edit below this line
