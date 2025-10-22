1eb4a6825b108b70007b55282e3ee3420d56f7c2
wilkye <wilkylegreer@gmail.com>
2025-10-22 14:12:53 -0600

    Exercise 12 complete

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let total = 1;
+  for (let i = 0; i < arr.length; i++) {
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n < 0) {
+    return "Factorial is not defined for negative numbers.";
+  } else if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
