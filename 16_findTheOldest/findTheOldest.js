e5bc37501d42708aee306825052253da8ceb4b6d
wilkye <wilkylegreer@gmail.com>
2025-10-23 17:26:23 -0600

    Exercise 16 complete

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    if (!arr || arr.length === 0) return undefined;
 
+    const getAge = person => {
+        const birth = person.yearOfBirth;
+        const death = (person.yearOfDeath === undefined) ? new Date().getFullYear() : person.yearOfDeath;
+        return death - birth;
+    };
+
+    let finalIndex = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (getAge(arr[i]) > getAge(arr[finalIndex])) {
+            finalIndex = i;
+        }
+    }
+
+    return arr[finalIndex];
 };
 
 // Do not edit below this line
