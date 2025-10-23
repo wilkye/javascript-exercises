a57cd714a0b3a0e8cbdb2d4a820f3e9fa603ff3c
wilkye <wilkylegreer@gmail.com>
2025-10-23 16:44:06 -0600

    Exercise 14 complete

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function (x) {
+    x = parseInt(x);
+    if (x == 0) {
+        return x
+    } else if (x < 0) {
+        return "OOPS";
+    }
+    let fibArray = [];
+    for (let i = 0; i < x; i++) {
+        if (i <= 1) {
+            fibArray.push(1);
+        } else {
+            const a = fibArray[i - 1];
+            const b = fibArray[i - 2];
+            let total = b + a;
+            fibArray.push(total);
+        }
+    }
+    return fibArray[x - 1]
 };
 
 // Do not edit below this line
