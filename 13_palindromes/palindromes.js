4ea1d77bb8ba146c446521ff08ef9950df72ec99
wilkye <wilkylegreer@gmail.com>
2025-10-23 16:23:23 -0600

    Exercise 13 completed, not the most elegant but hey, it works

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const reversed = str
+        .split('')
+        .reverse()
+        .join('')
+        .toLowerCase()
+        .replaceAll(" ", "")
+        .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
 
+    if (str.toLowerCase().replaceAll(" ", "").replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '') === reversed) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
